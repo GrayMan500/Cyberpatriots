aec9050c8fa3f37f2fe764e323ffd2a821adc1e6
GrayMan500 <69886247+GrayMan500@users.noreply.github.com>
2020-11-13 10:43:24 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+apt-get install -y libpam-cracklib
+apt-get install node.js
+apt-get remove zenmap
+apt-get remove zenmap nmap
+apt-get remove pure-ftpd
+apt-get install gufw
+gufw
+apt-get update
+apt-get upgrade
